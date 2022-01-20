025c9f5615120e5d3e5ab0557d06f418c9df9bcf
Henry Joerg <henry.joerg@gmail.com>
2022-01-20 13:52:39 +0100

    trips can be saved to the db

--- src/CreateTrip.jsx
+++ src/CreateTrip.jsx
@@ -1,3 +1,45 @@
+import {Button, TextField} from "@mui/material";
+import {useState} from "react";
+import {saveTrip} from "./services/saveTrip";
+
+const baseUrl = process.env.REACT_APP_API_BASE_URL;
+
 export default function CreateTrip(props) {
+  const [tripTitle, setTripTitle] = useState('');
+  const [tripDescription, setTripDescription] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+
+  const [confirmationScreen, setConfirmationScreen] = useState(false);
+
+  return (
+    <>
+      {confirmationScreen === false ? (
+        <div>
+          <TextField label={'Trip title'} variant={'outlined'} value={tripTitle}
+                     onChange={(e) => setTripTitle(e.target.value)}/>
+          <TextField label={'Trip description'} variant={'outlined'} value={tripDescription}
+                     onChange={(e) => setTripDescription(e.target.value)}/>
+          <TextField type={'date'} variant="outlined" value={startDate}
+                     onChange={(e) => setStartDate(e.target.value)}/>
+          <TextField type={'date'} variant="outlined" value={endDate}
+                     onChange={(e) => setEndDate(e.target.value)}/>
+          <Button variant={'outlined'} onClick={() => setConfirmationScreen(true)} disabled={ !tripTitle || !tripDescription ||
+            !startDate || !endDate }>continue</Button>
+        </div>
+      ) : (
+        <div>
+          <p>{tripTitle}</p>
+          <p>{tripDescription}</p>
+          <p>{startDate}</p>
+          <p>{endDate}</p>
+          <div>
+            <Button variant={'outlined'} onClick={() => setConfirmationScreen(false)}>back</Button>
+            <Button variant={'outlined'} onClick={() => saveTrip(tripTitle, tripDescription, startDate, endDate)}>confirm</Button>
+          </div>
+        </div>
+      )}
+    </>
+  )
 
 }
