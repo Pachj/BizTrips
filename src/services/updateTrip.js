f554ce18b97612f807700f601cad0c22d942d300
Henry Joerg <henry.joerg@gmail.com>
2022-01-24 11:37:23 +0100

    updateTrip fixed

--- src/services/updateTrip.js
+++ src/services/updateTrip.js
@@ -1,6 +1,20 @@
+import formatDateForDb from "./commons";
+import {format, isValid} from "date-fns";
+
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
 export async function updateTrip(id, tripTitle, tripDescription, startDate, endDate) {
+  if (isValid(startDate)) {
+    console.log('start');
+    console.log(startDate);
+    startDate = format(startDate, 'yyyy-MM-dd');
+  }
+  if (isValid(endDate)) {
+    console.log('end');
+    console.log(endDate);
+    endDate = format(endDate, 'yyyy-MM-dd');
+  }
+
   return fetch(baseUrl + 'trips/' + id, {
     method: "PUT",
       headers: {
@@ -10,8 +24,8 @@ export async function updateTrip(id, tripTitle, tripDescription, startDate, endD
       JSON.stringify({
         "title": tripTitle,
         "description": tripDescription,
-        "startTrip": startDate,
-        "endTrip": endDate
+        "startTrip": formatDateForDb(startDate),
+        "endTrip": formatDateForDb(endDate)
       })
   });
 }
